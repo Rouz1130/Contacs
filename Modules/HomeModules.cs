6b7b1dfe9903e0d65b475101a9f8593b3ffaea4d
Rouz Majlessi <rmajlessi@gmail.com>
2016-07-15 16:11:18 -0700

    fix dnx errors

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -10,25 +10,24 @@ namespace AddressBook
     {
       Get["/"] = _ => {
         return View["index.cshtml"];
-      }:
-
-      Get["/display-contact"] = _ => {
-        return View["display-contact.cshtml"];
       };
 
-      Get]["/new_contact"] = _ => {
+      Get["/display_contact"] = _ => {
+        return View["display_contact.cshtml"];
+      };
+
+      Get["/new_contact"] = _ => {
         return View["new_contact.cshtml"];
       };
 
-
-
       Post["/added_contact"] = _ => {
-        Contact newContact = new Contact(Request.Form["contact-name"]; Requst.Form["contact_phoneNumber"], Request.Form["contact-address"]);
+        Contact newContact = new Contact(Request.Form["contact-name"]; Requst.Form["contact-phoneNumber"], Request.Form["contact-address"]);
         return View["added_contact.cshtml", newContact];    };
 
         Post["/deleted_contact"] = _ => {
           Contact.ClearAll();
-          return View["deleted_deleted.cshtml"];
+          return View["deleted_contact.cshtml"];
         };
   }
+ }
 }

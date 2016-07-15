2f7ecec3a149499b33082b531fabf1c7ca0b381f
Rouz Majlessi <rmajlessi@gmail.com>
2016-07-14 23:29:58 -0700

    object file set named class & set strings

---
+++ Objects/Contact.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class Contact
+  {
+    private string _name;
+    private int _phoneNumber;
+    private string _address;
+    private int _id;
+    private static List<Contact> _instances = new List<Contact>{};
+
+    public Contact(string name, int phoneNumber, string address)
+    {
+
+
+      
+    }
+
+
+  }
+
+
+
+
+
+
+
+}
